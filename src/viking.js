4bf3b5d645e784bdcef8aba2975287e84cf20672
Ernesto Espinoza M <emenocal07@gmail.com>
2022-01-16 21:07:30 +0100

    beginning practice

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,32 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health,
+    this.strength = strength
+
+  }
+
+  attack() {
+    return this.strength
+  };
+
+  receivedDamage(theDamage) {
+    return this.health -= theDamage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength){
+    this.name = name,
+  }
+ }
 
 // Saxon
-class Saxon {}
+class Saxon { }
 
 // War
-class War {}
+class War { }
 
 
 
